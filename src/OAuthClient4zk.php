6823fe8c9dacf2ce70e0868b2dd49f2af31d82a7
Zhiyuan Zou <zhiyuan.zou@nokia-sbell.com>
2018-05-31 22:39:04 -0500

    Remove some redundant function in OAuthclient4zk.php...

--- src/OAuthClient4zk.php
+++ src/OAuthClient4zk.php
@@ -117,86 +117,6 @@ class OAuthClient4zk
     }
 
     /**
-     * Perform a GET request, convenience wrapper for ::send().
-     *
-     * @param string $requestScope
-     * @param string $requestUri
-     * @param array  $requestHeaders
-     *
-     * @return Http\Response|false
-     */
-    public function get($requestScope, $requestUri, array $requestHeaders = [])
-    {
-        return $this->send($requestScope, Request::get($requestUri, $requestHeaders));
-    }
-
-    /**
-     * Perform a POST request, convenience wrapper for ::send().
-     *
-     * @param string $requestScope
-     * @param string $requestUri
-     * @param array  $postBody
-     * @param array  $requestHeaders
-     *
-     * @return Http\Response|false
-     */
-    public function post($requestScope, $requestUri, array $postBody, array $requestHeaders = [])
-    {
-        return $this->send($requestScope, Request::post($requestUri, $postBody, $requestHeaders));
-    }
-
-    /**
-     * Perform a HTTP request.
-     *
-     * @param string       $requestScope
-     * @param Http\Request $request
-     *
-     * @return Response|false
-     */
-    public function send($requestScope, Request $request)
-    {
-        if (null === $this->userId) {
-            throw new OAuthException('userId not set');
-        }
-
-        $accessToken = $this->getAccessToken($requestScope);
-        if (!$accessToken) {
-            return false;
-        }
-
-        if ($accessToken->isExpired($this->dateTime)) {
-            // access_token is expired, try to refresh it
-            if (null === $accessToken->getRefreshToken()) {
-                // we do not have a refresh_token, delete this access token, it
-                // is useless now...
-                $this->tokenStorage->deleteAccessToken($this->userId, $accessToken);
-                return false;
-            }
-
-            // try to refresh the AccessToken
-            $accessToken = $this->refreshAccessToken($accessToken);
-            if (!$accessToken) {
-                // didn't work
-                return false;
-            }
-        }
-
-        // add Authorization header to the request headers
-        $request->setHeader('Authorization', sprintf('Bearer %s', $accessToken->getToken()));
-
-        $response = $this->httpClient->send($request);
-        if (401 === $response->getStatusCode()) {
-            // the access_token was not accepted, but isn't expired, we assume
-            // the user revoked it, also no need to try with refresh_token
-            $this->tokenStorage->deleteAccessToken($this->userId, $accessToken);
-
-            return false;
-        }
-
-        return $response;
-    }
-
-    /**
      * Obtain an authorization request URL to start the authorization process
      * at the OAuth provider.
      *
@@ -251,229 +171,6 @@ class OAuthClient4zk
     }
 
     /**
-     * @param string $responseCode  the code passed to the "code" query parameter on the callback URL
-     * @param string $responseState the state passed to the "state" query parameter on the callback URL
-     *
-     * @return void
-     */
-    public function handleCallback($responseCode, $responseState)
-    {
-        if (null === $this->userId) {
-            throw new OAuthException('userId not set');
-        }
-
-        // get and delete the OAuth session information
-        $sessionData = $this->session->take('_oauth2_session');
-
-        if (!hash_equals($sessionData['state'], $responseState)) {
-            // the OAuth state from the initial request MUST be the same as the
-            // state used by the response
-            throw new OAuthException('invalid session (state)');
-        }
-
-        // session providerId MUST match current set Provider
-        if ($sessionData['provider_id'] !== $this->provider->getProviderId()) {
-            throw new OAuthException('invalid session (provider_id)');
-        }
-
-        // session userId MUST match current set userId
-        if ($sessionData['user_id'] !== $this->userId) {
-            throw new OAuthException('invalid session (user_id)');
-        }
-
-        // prepare access_token request
-        $tokenRequestData = [
-            'client_id' => $this->provider->getClientId(),
-            'grant_type' => 'authorization_code',
-            'code' => $responseCode,
-            'redirect_uri' => $sessionData['redirect_uri'],
-            'code_verifier' => $sessionData['code_verifier'],
-        ];
-
-        $requestHeaders = [];
-        // if we have a secret registered for the client, use it
-        if (null !== $this->provider->getSecret()) {
-            $requestHeaders = [
-                'Authorization' => sprintf(
-                    'Basic %s',
-                    Base64::encode(
-                        sprintf('%s:%s', $this->provider->getClientId(), $this->provider->getSecret())
-                    )
-                ),
-            ];
-        }
-
-        $response = $this->httpClient->send(
-            Request::post(
-                $this->provider->getTokenEndpoint(),
-                $tokenRequestData,
-                $requestHeaders
-            )
-        );
-
-        if (400 === $response->getStatusCode()) {
-            // check for "invalid_grant"
-            $responseData = $response->json();
-            if (!array_key_exists('error', $responseData) || 'invalid_grant' !== $responseData['error']) {
-                // not an "invalid_grant", we can't deal with this here...
-                throw new OAuthServerException($response);
-            }
-
-            throw new OAuthException('authorization_code was not accepted by the server');
-        }
-
-        if (!$response->isOkay()) {
-            // if there is any other error, we can't deal with this here...
-            throw new OAuthServerException($response);
-        }
-        $this->tokenStorage->storeAccessToken(
-            $this->userId,
-            AccessToken::fromCodeResponse(
-                $this->provider,
-                $this->dateTime,
-                $response->json(),
-                // in case server does not return a scope, we know it granted
-                // our requested scope
-                $sessionData['scope']
-            )
-        );
-    }
-
-    /**
-     * Verify if an AccessToken in the list that matches this scope, bound to
-     * providerId and userId.
-     *
-     * This method has NO side effects, i.e. it will not try to use, refresh or
-     * delete AccessTokens. If a token is expired, but a refresh token is
-     * available it is assumed that an AccessToken is available.
-     *
-     * NOTE: this does not mean that the token will also be accepted by the
-     * resource server!
-     *
-     * @param string $scope
-     *
-     * @return bool
-     */
-    public function hasAccessToken($scope)
-    {
-        if (false === $accessToken = $this->getAccessToken($scope)) {
-            return false;
-        }
-
-        // is it expired? but do we have a refresh_token?
-        if ($accessToken->isExpired($this->dateTime)) {
-            // access_token is expired
-            if (null !== $accessToken->getRefreshToken()) {
-                // but we have a refresh_token
-                return true;
-            }
-
-            // no refresh_token
-            return false;
-        }
-
-        // not expired
-        return true;
-    }
-
-    /**
-     * @param AccessToken $accessToken
-     *
-     * @return AccessToken|false
-     */
-    private function refreshAccessToken(AccessToken $accessToken)
-    {
-        // prepare access_token request
-        $tokenRequestData = [
-            'grant_type' => 'refresh_token',
-            'refresh_token' => $accessToken->getRefreshToken(),
-            'scope' => $accessToken->getScope(),
-        ];
-
-        $requestHeaders = [];
-        // if we have a secret registered for the client, use it
-        if (null !== $this->provider->getSecret()) {
-            $requestHeaders = [
-                'Authorization' => sprintf(
-                    'Basic %s',
-                    Base64::encode(
-                        sprintf('%s:%s', $this->provider->getClientId(), $this->provider->getSecret())
-                    )
-                ),
-            ];
-        }
-
-        $response = $this->httpClient->send(
-            Request::post(
-                $this->provider->getTokenEndpoint(),
-                $tokenRequestData,
-                $requestHeaders
-            )
-        );
-
-        if (400 === $response->getStatusCode()) {
-            // check for "invalid_grant"
-            $responseData = $response->json();
-            if (!array_key_exists('error', $responseData) || 'invalid_grant' !== $responseData['error']) {
-                // not an "invalid_grant", we can't deal with this here...
-                throw new OAuthServerException($response);
-            }
-
-            // delete the access_token, we assume the user revoked it
-            $this->tokenStorage->deleteAccessToken($this->userId, $accessToken);
-
-            return false;
-        }
-
-        if (!$response->isOkay()) {
-            // if there is any other error, we can't deal with this here...
-            throw new OAuthServerException($response);
-        }
-
-        // delete old AccessToken as we'll write a new one anyway...
-        $this->tokenStorage->deleteAccessToken($this->userId, $accessToken);
-
-        $accessToken = AccessToken::fromRefreshResponse(
-            $this->provider,
-            $this->dateTime,
-            $response->json(),
-            // provide the old AccessToken to borrow some fields if the server
-            // does not provide them on "refresh"
-            $accessToken
-        );
-
-        // store the refreshed AccessToken
-        $this->tokenStorage->storeAccessToken($this->userId, $accessToken);
-
-        return $accessToken;
-    }
-
-    /**
-     * Find an AccessToken in the list that matches this scope, bound to
-     * providerId and userId.
-     *
-     * @param string $scope
-     *
-     * @return AccessToken|false
-     */
-    public function getAccessToken($scope)
-    {
-        $accessTokenList = $this->tokenStorage->getAccessTokenList($this->userId);
-        foreach ($accessTokenList as $accessToken) {
-            if ($this->provider->getProviderId() !== $accessToken->getProviderId()) {
-                continue;
-            }
-            if ($scope !== $accessToken->getScope()) {
-                continue;
-            }
-
-            return $accessToken;
-        }
-
-        return false;
-    }
-
-    /**
      * @param string $codeVerifier
      *
      * @return string
@@ -512,7 +209,7 @@ class OAuthClient4zk
     //-------------------------------------------------
     // 1.The following to TALK with keycloak-server.
     //-------------------------------------------------
-    public function logout4kc($sessionid)
+    public function logout($sessionid)
     {
         $refresh_token = $this->getAccessToken4kc($sessionid)->getRefreshToken();
         $RequestData = [
@@ -604,7 +301,7 @@ class OAuthClient4zk
     //-------------------------------------------------
     // 2.@The following to DELETE|GET|STORE|UPDATE Token DATA from keycloak via ZBX default DataBase
     //-------------------------------------------------
-    public function hasAccessToken4kc($sessionid)
+    public function hasAccessToken($sessionid)
     {
         if (false === $accessToken = $this->getAccessToken4kc($sessionid)) {
             return false;
@@ -627,7 +324,7 @@ class OAuthClient4zk
         return true;
     }
 
-    public function getAccessToken4kc($sessionid)
+    public function getAccessToken($sessionid)
     {
         $accessToken = $this->tokenStorage->getAccessTokenList($sessionid);                                                                                                               
         if (false == $accessToken) {
@@ -636,7 +333,7 @@ class OAuthClient4zk
         return $accessToken;                                                                                                                                                                 
     }
 
-    public function refreshAccessToken4kc(AccessToken $accessToken ,$sessionid)
+    public function refreshAccessToken(AccessToken $accessToken ,$sessionid)
     {
         // prepare access_token request
         $tokenRequestData = [
@@ -791,7 +488,7 @@ class OAuthClient4zk
     //-------------------------------------------------
     // 2.@The following to DELETE|GET|STORE|UPDATE Token DATA from keycloak via ZBX default DataBase
     //-------------------------------------------------
-    public function handleCallback4kc($responseCode, $responseState)
+    public function handleCallback($responseCode, $responseState)
     {
         if (null === $this->userId) {
             throw new OAuthException('userId not set');
